85e9ce1dba5338b4a5cb95f6abf554c3a6b6785e
Ari <lee_ari95@icloud.com>
2024-07-27 17:26:27 +0900

    fix: 워크플로우 에러 수정

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AriNote",
+    platforms: [.macOS(.v14)],
     products: [
         .library(name: "AriNote", targets: ["AriNote"]),
         .executable(name: "Scripts", targets: ["Scripts"]),
--- Sources/AriNote/TIL.docc/SwiftUI.md
+++ Sources/AriNote/TIL.docc/SwiftUI.md
@@ -6,12 +6,4 @@
    @TitleHeading("Overview")
 }
 
-## Overview
-
-d
-
 ## Topics
-- <doc:220317_Widget,_Timeline,_Decorator_Pattern>
-- <doc:220315_Memory_Leak,_RxSwift,_withUnretained,_Widget>
-- <doc:220224_SwiftUI,_Property_Wrapper,_Divider,_List,_GeometryReader>
-- <doc:220221_Localization,_퀴즈_오답노트,_SwiftUI_Tutorial>
